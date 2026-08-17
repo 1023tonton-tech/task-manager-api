9af0b9aeb6e2a1f3e56a366e22d1ac8b932ef106
1023tonton-tech <267451451+1023tonton-tech@users.noreply.github.com>
2026-08-18 01:56:12 +0900

    Add task list endpoint

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+tasks = [
+    {
+        "id": 1,
+        "title": "レポートを提出する",
+        "completed": False,
+    }
+]
 @app.get("/")
 def read_root():
     return {"message": "Task Manager API"}
+
+
+@app.get("/tasks")
+def read_tasks():
+    return tasks
